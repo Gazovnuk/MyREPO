9db7afa585b9a2a3253b93a1037fbd9b9ccf518b
Alexey Litvinov <litvinov-ae@yandex.ru>
2020-02-26 17:16:24 +0400

    add else algorithms

--- src/Search.java
+++ src/Search.java
@@ -23,4 +23,27 @@ public class Search {
         }
         return -1;
     }
+
+    public static int binarySearchRecursive(int[] sortedArray, int key, int low, int high) {
+        // считаем индекс центрального элемента
+        int middle = low + (high - low) / 2;
+
+        // base case (условие выхода) - регион поиска пустой
+        if (low > high) {
+            // не нашли элемента, который равен ключу
+            return -1;
+        }
+
+        if (key == sortedArray[middle]) {
+            // в случае, если элемент в центре равняется ключу (нашли элемент)
+            return middle;
+        } else if (key < sortedArray[middle]) {
+            // рекурсивный вызов для левого подмассива
+            // не забывайте здесь ключевое слово return (подумайте, зачем оно нужно)
+            return binarySearchRecursive(sortedArray, key, low, middle - 1);
+        } else {
+            // рекурсивный вызов для правого помассива
+            return binarySearchRecursive(sortedArray, key, middle + 1, high);
+        }
+    }
 }
